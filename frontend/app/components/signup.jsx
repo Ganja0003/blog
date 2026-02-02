b541a97e4a6ad918babd7cb23840184f64cb3358
yuusuf <yuusuf98@outlook.com>
2026-02-02 17:44:51 +0100

    Send a post request to backend with signUp info

--- frontend/app/components/signup.jsx
+++ frontend/app/components/signup.jsx
@@ -1,21 +1,64 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+
 export default function Signup() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  function handleFormData(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  console.log(formData);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const res = await fetch("http://127.0.0.1:3001/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    if (res.ok) {
+      alert("User created");
+    }
+  }
   return (
-    <form className="signUpForm">
-        <h1>SignUp</h1>
+    <form className="signUpForm" onSubmit={handleSubmit}>
+      <h1>SignUp</h1>
       <div className="nameSignupContainer">
         <label htmlFor="name">Name:</label>
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleFormData}
+        />
       </div>
 
       <div className="emailSignupContainer">
         <label htmlFor="email">Email:</label>
-        <input type="text" name="email" />
+        <input
+          type="text"
+          name="email"
+          value={formData.email}
+          onChange={handleFormData}
+        />
       </div>
 
       <div className="passwordSignupContainer">
         <label htmlFor="password">Password:</label>
-        <input type="text" name="password" />
+        <input
+          type="text"
+          name="password"
+          value={formData.password}
+          onChange={handleFormData}
+        />
       </div>
 
       <button type="submit">Submit</button>
