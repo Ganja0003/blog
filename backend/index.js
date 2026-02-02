8def72064054383386b18790efd703404d75bd1c
yuusuf <yuusuf98@outlook.com>
2026-02-02 17:43:53 +0100

    add cors to allow frontend requests

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import db from './database.js'
 import authRoute from './auth/route.js'
+import cors from 'cors'
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 app.use('/auth',authRoute)
 
 app.get('/',(req,res) => {
