23bc9725a788e69851821da2ed3b6c110d9e7350
yuusuf <yuusuf98@outlook.com>
2026-02-05 16:29:49 +0100

    Connect frontend to backend by sending a post req with login info, and redirect to profile page if the user enters the right credentials.

--- frontend/app/components/login.jsx
+++ frontend/app/components/login.jsx
@@ -1,18 +1,69 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const router = useRouter();
+
+  function handleFormData(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://127.0.0.1:3001/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        console.log("Logged in", data);
+        router.push("/profile");
+      }else{
+        console.log('Login failed',data);
+      }
+      
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <>
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={handleSubmit}>
         <h1>Login</h1>
         <div className="emailLoginContainer">
           <label htmlFor="email">Email:</label>
-          <input type="text" name="email" />
+          <input
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleFormData}
+          />
         </div>
 
         <div className="passwordLoginContainer">
           <label htmlFor="password">Password:</label>
-          <input type="text" name="password" />
+          <input
+            type="text"
+            name="password"
+            value={formData.password}
+            onChange={handleFormData}
+          />
         </div>
 
         <button type="submit">Submit</button>
---
+++ frontend/app/profile/page.jsx
@@ -0,0 +1,5 @@
+export default function(){
+    return(
+        <h1>Welcome to you profile</h1>
+    );
+}
