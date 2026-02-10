ee9686ad7c988bae91888b798af174edd7bd3dcb
yuusuf <yuusuf98@outlook.com>
2026-02-10 13:12:22 +0100

    User redirected to home page instead of their profile.

--- frontend/app/components/login.jsx
+++ frontend/app/components/login.jsx
@@ -34,7 +34,7 @@ export default function Login() {
       const data = await res.json();
       if (res.ok) {
         console.log("Logged in", data);
-        router.push("/profile");
+        router.push("/");
       }else{
         console.log('Login failed',data);
       }
