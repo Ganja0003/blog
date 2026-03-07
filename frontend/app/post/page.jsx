5856aff2c36e1d184486efed8197f56dbf72181b
yuusuf <yuusuf98@outlook.com>
2026-03-07 16:40:31 +0100

    server component of create post page that gets the from the cookie.

--- frontend/app/post/page.jsx
+++ frontend/app/post/page.jsx
@@ -1,33 +1,9 @@
-import Loggedin from "../components/navbar/loggedIn";
 import {cookies} from 'next/headers'
+import PostClient from "./postClient";
 
 export default async function Post(){
 const cookieHolder = await cookies();
-const token = cookieHolder.get('token')
+const token = cookieHolder.get('token');
 
-if(!token){
-    return <h1>please login</h1>
-}
-
-
-    return(
-    <>
-    <Loggedin/>
-    <div className="createPostContainer">
-        <h1>Create Post</h1>
-        <form className="createPostForm">
-            <div className="createPostFormChild">
-                <label htmlFor="title">Title</label>
-                <input type="text" id='title'/>
-            </div>
-            <div className="createPostFormChild">
-                <label htmlFor="content">Content</label>
-                <textarea id='content' rows="8" cols='33'/>
-            </div>
-            
-        </form>
-    </div>
-    
-    </>
-);
+    return <PostClient token={token}/>;
 }
