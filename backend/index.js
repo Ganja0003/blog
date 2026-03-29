16a82bc63ceadda97a722c3031bd22938e3ccad1
yuusuf <yuusuf98@outlook.com>
2026-03-29 23:00:15 +0200

    fix

--- backend/index.js
+++ backend/index.js
@@ -84,6 +84,8 @@ app.delete('/posts/:id',validateToken, async(req,res)=>{
     }
 })
 
-app.listen(process.env.PORT, () => {
-    console.log('Server is running on port');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
