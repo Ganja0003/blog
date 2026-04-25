17550edf0fe4028fae001a4e20d21feaf438b3e9
yuusuf <yuusuf98@outlook.com>
2026-04-25 14:07:03 +0200

    use cookie based auth instead of header based auth in backend

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,15 +2,12 @@ import jwt from 'jsonwebtoken'
 
 
 function validateToken(req,res,next){
- const authHeader = req.headers['authorization'];
+ const token = req.cookies.token;
  
- if (!authHeader) return res.status(401).json('User not authenticated');
-
- const token = authHeader.split(' ')[1]
-
- if(!token){
-    res.status(400).json('user not authenticated')
- }
+ if (!token) {
+    return res.status(401).json('User not authenticated');
+  }
+  
 try{
  const validToken = jwt.verify(token, process.env.JWT_SECRET);
  if(validToken){
