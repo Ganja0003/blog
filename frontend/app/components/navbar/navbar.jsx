68f122599ec518023436348f842e2d5d005d9d25
yuusuf <yuusuf98@outlook.com>
2026-05-20 15:21:15 +0200

    fix

--- frontend/app/components/navbar/navbar.jsx
+++ frontend/app/components/navbar/navbar.jsx
@@ -4,23 +4,25 @@ import LoggedIn from "./loggedIn";
 import LoggedOut from "./loggedOut";
 
 export default function Navbar() {
-  const [authenticated, setAuthenticated] = useState[null];
+  const [authenticated, setAuthenticated] = useState(null);
   const URL = "https://easygoing-imagination-production-0598.up.railway.app";
 
   useEffect(() => {
     async function auth() {
       try {
-        const res = await fetch(`${URL}/profile`);
+        const res = await fetch(`${URL}/profile`, {
+            credentials: 'include'
+        });
         setAuthenticated(res.ok)
       } catch (err) {
         console.log(err);
         setAuthenticated(false);
       }
-    } userAuth()
+    } auth()
   }, []);
 
   if(authenticated === null){
-    <p>loading ....</p>
+    return <p>loading ....</p>
   }
 
   return authenticated ? <LoggedIn/> : <LoggedOut/>;
