15fdbe27f812c8aec1883e74e9f85903ea1481ee
yuusuf <yuusuf98@outlook.com>
2026-05-20 15:08:51 +0200

    use the new navbar authcheck for home page

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,19 +1,14 @@
 import {cookies} from 'next/headers'
-import LoggedOut from './components/navbar/loggedOut';
-import LoggedIn from './components/navbar/loggedIn';
+import Navbar from './components/navbar/navbar';
+
 import AllPosts from './components/allPosts'
 
 export const dynamic = "force-dynamic";
 
 export default async function Home(){
-    const cookieReq = await cookies();
-    const token = cookieReq.get('token')?.value;
-    console.log(token)
-    console.log('testing')
     return(
         <>
-        {!token && <LoggedOut/>}
-        {token && <LoggedIn/>}
+        {<Navbar/>}
         {<AllPosts/>}
         </>
     );
